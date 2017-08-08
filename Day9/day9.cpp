9e27af6639a6b53aaa57c965b764963ce125b5c6
Shifali Gupta <shifali1998@gmail.com>
2017-08-09 00:17:15 +0530

    Added Day9 Solution in C++

---
+++ Day9/day9.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n) {
+    // Complete this function
+    if(n==0 || n==1)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int result = factorial(n);
+    cout << result << endl;
+    return 0;
+}
