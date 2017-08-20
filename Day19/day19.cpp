52262774db20f037a45dc42b2542b5cd14f086e2
Shifali Gupta <shifali1998@gmail.com>
2017-08-20 22:45:08 +0530

    Added Day19 Solution in C++

---
+++ Day19/day19.cpp
@@ -0,0 +1,14 @@
+//Write your code here
+class Calculator : public AdvancedArithmetic
+{
+    int divisorSum(int n)
+    {
+        int sum = 0;
+        for(int i=1; i<=n; i++)
+        {
+            if(n%i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+};
